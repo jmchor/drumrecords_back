a2d75dc63ed8f495668052af7c585c8e23f0f3f5
Johannes Chorzempa <jchorzempa@posteo.org>
2023-06-27 15:14:22 +0200

    edit auth routs route

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 
 const randomRoute = require("./routes/random.routes");
 app.use("/", randomRoute);
